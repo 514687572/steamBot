cb4a2b7abd1b0db8b8f7c2d37e73026489229fcd
514687572 <514687572@qq.com>
2017-08-24 14:22:34 +0800

    重构项目
    重构项目

---
+++ steamBot/src/main/java/com/stip/net/controller/AbstractController.java
@@ -0,0 +1,11 @@
+package com.stip.net.controller;
+
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class AbstractController {
+	public static Log log = LogFactory.getLog(AbstractController.class);
+	public Map<String,Object> result;
+}
---
+++ steamBot/src/main/java/com/stip/net/utils/Constant.java
@@ -0,0 +1,5 @@
+package com.stip.net.utils;
+
+public class Constant {
+	public static String steamKey="3E490F5A522707F6692E80D12ACE45AB";
+}
